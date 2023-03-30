89e607d25857841b1b97cd90130847078b94cc32
00jesus <j.acevedo03@ufromail.cl>
2023-03-30 15:35:16 -0300

    Metodo agregar estudiante y validacion de double entre 1.0 y 7.0

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         double [][] matriz = iniciarMatriz(50, 5);
         System.out.println(matriz);
         mostrarMatriz(matriz);
-        notasAleatorias(matriz);
+        agregarEstudiante(matriz);
         mostrarMatriz(matriz);
 // 5 notas x 50 estudiantes
     }
-
+    public static double validarDouble () {
+        Scanner teclado = new Scanner(System.in);
+        Double valor = null;
+        while (valor == null || valor < 1 || valor > 7) {
+                try {
+                    valor = Double.parseDouble(teclado.nextLine());
+                    if (valor <1 || valor >7) {
+                        System.out.println("Ingrese valores entre 1.0 y 7.0");
+                        }
+                    }
+                catch (Exception ex) {
+                    System.out.println("ERROR. Introduzca valores numericos");
+                }
+            }
+        return valor;
+    }
+    public static double[][] agregarEstudiante (double [][]matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("Estudiante: "+(i+1));
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == 0.0) {
+                    matriz[i][j] = validarDouble();
+                    System.out.println(matriz[i][j]+", ");
+                } else if (matriz[i][j] != 0) {
+                    System.out.println("ya hay nota en estudiante: "+(i+1)+" nota n°:"+(j+1));
+                }
+            }
+        }
+        return matriz;
+    }
     public static double [][] notasAleatorias (double [][] mat) {
         for (int i = 0; i <mat.length ; i++) {
             for (int j = 0; j < mat[i].length; j++) {
