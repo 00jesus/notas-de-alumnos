67c90d54ba1a14f5fed2671a32bafd83d1c3a8c6
00jesus <j.acevedo03@ufromail.cl>
2023-03-30 16:29:36 -0300

    Proyecto finalizado, con la mayoria de metodos creados

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,69 @@
 import java.util.Scanner;
 
+// 5 notas x 50 estudiantes
 public class Main {
     public static void main(String[] args) {
+        menu();
 
-        double [][] matriz = iniciarMatriz(50, 5);
-        System.out.println(matriz);
-        mostrarMatriz(matriz);
-        agregarEstudiante(matriz);
-        mostrarMatriz(matriz);
-// 5 notas x 50 estudiantes
+    }
+    public static void menu () {
+        Scanner entrada = new Scanner(System.in);
+        int resp = -1;
+        double [][] matriz = iniciarMatriz(50,5);
+        while (resp!=0) {
+            System.out.println("""
+                    1 Agregar estudiantes
+                    2 Mostrar aprobados, reprobados, examenes
+                    3 Llenar con notas aleatorias
+                    4 Mostrar alumnos(vertical) y notas(horizontal)""");
+            System.out.print("Ingrese opcion: ");
+            resp = entrada.nextInt();
+
+            if (resp == 1) agregarEstudiante(matriz);
+            if (resp == 2) apruebanReprueban(matriz);
+            if (resp ==3) notaAleatoria(matriz);
+            if (resp ==4) mostrarMatriz(matriz);
+            if (resp == 5 ) calcularPromedios(matriz);
+
+            else {
+                System.out.println(" "); //solo espacio
+            }
+        }
+    }
+    public static void apruebanReprueban (double [][] matriz) {
+        int aprueban = 0;
+        int reprueban = 0;
+        int examen = 0;
+        double aux;
+        for (int i = 0; i < matriz.length; i++) {
+            aux = calcularPromedio(matriz, i);
+            if (aux >= 4) {
+                aprueban = aprueban + 1;
+            } else if (aux>3.5 && aux<4) {
+                examen = examen +1;
+            }else if (aux<3.6) {
+                reprueban = reprueban +1;
+            }
+        }
+        System.out.println("Reprueban: "+ reprueban);
+        System.out.println("Aprueban: "+ aprueban);
+        System.out.println("Examen: "+ examen);
+    }
+    public static void calcularPromedios(double[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("promedio de alumno:"+(i+1)+ calcularPromedio(matriz,i));
+        }
+    }
+    public static double calcularPromedio (double [][]matriz, int estudiante) {
+        double acumulador = 0;
+        double promedio = 0;
+        double nota1 = matriz[estudiante][0];
+        double nota2 = matriz[estudiante][1];
+        double nota3 = matriz[estudiante][2];
+        double nota4 = matriz[estudiante][3];
+        double nota5 = matriz[estudiante][4];
+        promedio = (nota1*25 + nota2*25 + nota3*25 + nota4*15 + nota5*10)/5;
+        return promedio;
     }
     public static double validarDouble () {
         Scanner teclado = new Scanner(System.in);
@@ -28,26 +83,26 @@ public class Main {
     }
     public static double[][] agregarEstudiante (double [][]matriz) {
         for (int i = 0; i < matriz.length; i++) {
-            System.out.println("Estudiante: "+(i+1));
+            System.out.print("Estudiante: "+(i+1));
             for (int j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] == 0.0) {
+                    System.out.print(" nota: "+(j+1)+": ");
                     matriz[i][j] = validarDouble();
-                    System.out.println(matriz[i][j]+", ");
                 } else if (matriz[i][j] != 0) {
-                    System.out.println("ya hay nota en estudiante: "+(i+1)+" nota n°:"+(j+1));
+                    System.out.println("Ya hay nota en estudiante: "+(i+1)+" nota n°:"+(j+1));
                 }
 
             }
         }
         return matriz;
     }
-    public static double [][] notasAleatorias (double [][] mat) {
-        for (int i = 0; i <mat.length ; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                mat[i][j] = (Math.random()*6+1);
+    public static double [][] notaAleatoria (double [][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = Math.random()*6+1;
             }
         }
-        return mat;
+        return matriz;
     }
     public static double [][] iniciarMatriz (int estudiantes, int notas) {
         return new double[estudiantes][notas];
